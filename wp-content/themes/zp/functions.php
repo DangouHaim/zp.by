47290f8d55728f205ed33df8407c18ca3d34fd91
DangouHaim <gholub1996@mail.ru>
2017-09-03 23:11:28 +0300

    Fixed some errors

--- wp-content/themes/zp/functions.php
+++ wp-content/themes/zp/functions.php
@@ -1,6 +1,6 @@
 <?
-require( get_template_directory() . '/php/functions.php' );
-require( get_template_directory() . '/php/taxanomy.php' );
-require( get_template_directory() . '/php/metaboxes.php' );
-require( get_template_directory() . '/php/ajax-handler.php' );
+require( get_template_directory() . '/inc/php/includes.php' );
+require( get_template_directory() . '/inc/php/taxanomy.php' );
+require( get_template_directory() . '/inc/php/metaboxes.php' );
+require( get_template_directory() . '/inc/php/ajax-handler.php' );
 add_theme_support( 'post-thumbnails' );
